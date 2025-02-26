8d99f094b408d6b1080dfcd9bd94a471ac8213f3
lasha <92566978+lashahostavs@users.noreply.github.com>
2025-02-27 01:10:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ document.querySelector("button").addEventListener("click", function (){
     const sakomisio = 22.25
     const gakofa = 100
     const fasi = jsfasi+(jsfasi*sakomisio/gakofa)
+    const damrgvalebuliFasi = Math.ceil(fasi / 5) * 5
     if(isNaN(jsfasi)){
         alert("რიცხვები ჩაწერე.");
         return;
     }
-    alert("შენ მოსა დაგიჯდება "+Math.ceil(fasi)+" ლარი")
+    alert("შენ მოსა დაგიჯდება "+damrgvalebuliFasi+" ლარი")
 })
